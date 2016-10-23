f305a9feb0f3d89dd3395659e409a9090fa7991e
Chunbo Yin <mcu530@126.com>
2016-10-23 12:55:04 +0800

    fix typo

--- hello_world.php
+++ hello_world.php
@@ -11,5 +11,9 @@ int main(int argc,char *argv[])
 {
 printf(¡°hello.\n¡±);
 printf(¡°he was a student.\n¡±);
+printf(¡°he was born in finland.\n¡±);
+printf(¡°he is very clever!\n¡±);
+
+
 return 0;
 }
