a2a01c001afcc308d973cc2f75c673af1997c209
Chunbo Yin <mcu530@126.com>
2016-10-23 14:19:27 +0800

    Add feature-C

--- hello_world.php
+++ hello_world.php
@@ -9,10 +9,9 @@ int main(int argc,char *argv[])
 {
 printf(¡°hello.\n¡±);
 printf(¡°he was a student.\n¡±);
-printf(¡°he was born in finland.\n¡±);
-printf(¡°he is very clever!\n¡±);
+
 return 0;
 }
 
 
-feature-C
+feature-C right
