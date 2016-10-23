a98a414c3dd257cd65538d5f199ddaf1e8286434
Chunbo Yin <mcu530@126.com>
2016-10-23 13:52:23 +0800

    First commit

--- hello_world.php
+++ hello_world.php
@@ -4,16 +4,10 @@ echo "Hello World!¹þ¹þsecond feature b feature---c SSSSS";
 >>>>>>> fix-B
 ?>
 
-
-
 #include<stdio.h>
 int main(int argc,char *argv[])
 {
 printf(¡°hello.\n¡±);
 printf(¡°he was a student.\n¡±);
-printf(¡°he was born in finland.\n¡±);
-printf(¡°he is very clever!\n¡±);
-
-
 return 0;
 }
